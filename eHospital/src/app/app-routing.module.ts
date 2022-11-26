ab7bbef12af588dba8192832ec976e4047f76803
kornel.toth <kornel.toth@glosdel.com>
2022-11-26 20:32:48 +0100

    add routing

--- eHospital/src/app/app-routing.module.ts
+++ eHospital/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { DoctorComponent } from './component/doctor/doctor.component';
+import { PatientComponent } from './component/patient/patient.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dashboard', children : [
+    {path: '', redirectTo: 'patient', pathMatch: 'full'},
+    {path: 'patient', component: PatientComponent},
+    {path: 'doctor', component: DoctorComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
